b0d05adb7ac6f1361ef6c7f93f64daaedccba60a
Mike <Info@MikePeralta.com>
2020-05-02 03:14:08 -0700

    Simplify a bit

--- mike.py
+++ mike.py
@@ -5,14 +5,45 @@ import hamming
 from bitarray import bitarray
 
 
-data_bits = input("Enter your data bits ===> ")
-codeword_with_overall = hamming.encode(bitarray(data_bits))
-
-codeword_without_overall = codeword_with_overall[1:]
+print("Welcome to simple Hamming SEC/DED encoding/decoding")
 
 
-print("Original data bits:", data_bits)
-print("Codeword:", codeword_without_overall)
+def the_loop():
+
+	did_anything = False
+
+	print()
+	data_bits = input("Enter some data bits to encode (blank to skip this part) ===> ")
+	if data_bits != "":
+
+		did_anything = True
+
+		codeword = hamming.encode(bitarray(data_bits))
+
+		print("Original data bits:", data_bits)
+		print("Codeword:", codeword_with_overall)
+		print()
+
+	print()
+	codeword = input("Enter a codeword to decode (blank to skip this part) ===> ")
+	if codeword != "":
+
+		did_anything = True
+
+		codeword = bitarray(codeword)
+		data_bits = hamming.decode(codeword)
+
+		print("Original codeword:", codeword)
+		print("Decoded data bits:", data_bits)
+		print()
+
+	return did_anything
+
+
+while the_loop():
+	pass
+print("Did nothing last loop; Quitting")
+
 
 
 
